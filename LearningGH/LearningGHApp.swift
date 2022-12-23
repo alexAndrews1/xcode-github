83d5da3a0f6744d85dbc6c43e4f7d256969c0edd
Alex Andrews <andrewsalex577@gmail.com>
2022-12-23 17:35:47 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alex Andrews on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
